543f5bea87c46d3bfa2ed9a8ad5a92dee97612dc
Ruoyu Wang <wangruoyu@ufl.edu>
2018-09-19 14:06:20 -0400

    cross_platform

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import platform
 import numpy as np
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
@@ -13,24 +15,38 @@ python setup.py build_ext --inplace
 
 # Determine current directory of this setup file to find our module
 CUR_DIR = os.path.dirname(__file__)
-opencv_libs_str = 'C:/lib/opencv-3-4-2-vc14vc15/build/x64/vc15/lib/opencv_world342.lib'
-opencv_incs_str = 'C:/lib/opencv-3-4-2-vc14vc15/build/include'
-# Parse into usable format for Extension call
-opencv_libs = [str(lib) for lib in opencv_libs_str.strip().split()]
-opencv_incs = [str(inc) for inc in opencv_incs_str.strip().split()]
+sys_name = platform.system()
+if sys_name == 'Linux':
+    opencv_libs = glob.glob('/usr/local/lib/libopencv*.so')
+    opencv_incs = ['/usr/local/include/']
+
+elif sys_name=='Windows':
+    opencv_libs_str = 'C:/lib/opencv-3-4-2-vc14vc15/build/x64/vc15/lib/opencv_world342.lib'
+    opencv_incs_str = 'C:/lib/opencv-3-4-2-vc14vc15/build/include'
+    # Parse into usable format for Extension call
+    opencv_libs = [str(lib) for lib in opencv_libs_str.strip().split()]
+    opencv_incs = [str(inc) for inc in opencv_incs_str.strip().split()]
 
 self_incs = [os.path.join(CUR_DIR, 'src'),
              os.path.join(CUR_DIR, 'src/cv2cg/include'),
              os.path.join(CUR_DIR, 'src/cv2cg/3rdparty/lch/include')]
 
-extensions = [
+if sys_name == 'Linux':
+    extensions = [
     Extension('apriltag._apriltag',
               sources=[os.path.join(CUR_DIR, '_cy_apriltag.pyx')],
               language='c++',
               include_dirs=[np.get_include()] + opencv_incs + self_incs,
-              extra_compile_args=['/O2'],#['-O3', '-w'],
-              extra_link_args=opencv_libs)
-]
+              extra_compile_args=['-O3', '-w'],
+              extra_link_args=opencv_libs)]
+elif sys_name == 'Windows':
+    extensions = [
+    Extension('apriltag._apriltag',
+              sources=[os.path.join(CUR_DIR, '_cy_apriltag.pyx')],
+              language='c++',
+              include_dirs=[np.get_include()] + opencv_incs + self_incs,
+              extra_compile_args=['/O2'],
+              extra_link_args=opencv_libs)]
 
 setup(
     name="apriltag._apriltag",
